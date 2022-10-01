e61d51a5810f93b2beac1a236a416f4a6e362c29
Débora Veras <deboraveras.inf1.ep@outlook.com>
2022-10-01 17:27:41 -0300

    PokeDex já funcionando

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ const featchPokemon = () => {
     const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
       const types = pokemon.types.map(typeInfo => typeInfo.type.name)
       accumulator += `
-        <li class="card">
-          <img class = "card-image ${types[0]}" alt = "${pokemon.name}" src="https://raw.githubusercontent.com/RafaelSilva2k22/PokemonImages/main/images/${pokemon.id}.png"/>
-          <h2 class = "card-title">${pokemon.id}${pokemon.name}</h2>
+        <li class="card ${types[0]}">
+          <img class = "card-image" alt = "${pokemon.name}" src="https://raw.githubusercontent.com/RafaelSilva2k22/PokemonImages/main/images/${pokemon.id}.png"/>
+          <h2 class = "card-title">${pokemon.id} ${pokemon.name}</h2>
           <p class = "card-subtitle">${types.join(' | ')}</p>
         </li>`
       return accumulator
     }, '')
-    console.log(lisPokemons)
+
+    const ul = document.querySelector('[data-js = "pokedex"]')
+    
+    ul.innerHTML = lisPokemons
   })
 }
 
