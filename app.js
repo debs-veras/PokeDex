8896d5575c5fa4397e90ca8381eaad586aab1aab
Débora Veras <deboraveras.inf1.ep@outlook.com>
2022-10-01 16:18:29 -0300

    lis realizadas

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const featchPokemon = () => {
-  const url = "https://pokeapi.co/api/v2/pokemon/"
-  fetch(url)
-  .then(response => response.jsons())
-  .then
+  const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
+  const pokemonPromises = []
+  
+  for(let i = 1; i <= 150; i++){
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+  }
+
+  Promise.all(pokemonPromises).then(pokemons => {
+    const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+      accumulator += `<li>${pokemon.name}</li>`
+      return accumulator
+    }, '')
+    console.log(lisPokemons)
+  })
 }
 
 featchPokemon()
